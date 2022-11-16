97adaf3b6a2e7ee72b870e10a3c3133c3e214026
abhirampjayan <abhiramp08@gmail.com>
2022-11-16 12:00:22 +0530

    done

--- MyContract.sol
+++ MyContract.sol
@@ -10,20 +10,33 @@ contract Ownable {
         _;
     }
 
-    constructor() public {
+    constructor() {
         owner = msg.sender;
     }
 }
 
-contract MyContract is Ownable {
+contract SecretVault {
     string secret;
 
-    constructor(string memory _secret) public {
+    constructor(string memory _secret) {
         secret = _secret;
+    }
+
+    function getSecret() public view returns (string memory) {
+        return secret;
+    }
+}
+
+contract MyContract is Ownable {
+    address secretVault;
+
+    constructor(string memory _secret) {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
         super;
     }
 
     function getSecret() public view onlyOwner returns (string memory) {
-        return secret;
+        return SecretVault(secretVault).getSecret();
     }
 }
